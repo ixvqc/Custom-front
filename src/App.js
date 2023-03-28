38941ba88c8d166f05f8ef9a0ce0bd00eabef2db
Pablito223 <portki14@gmail.com>
2023-03-28 21:38:43 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Main from './views/Main'
 import {Routes, Route} from 'react-router-dom'
 import Login from './views/Login'
+import Register from "./views/Register";
  function App() {
   return (
    <Routes>
      <Route path="/" element = {<Main/>}/>
        <Route path="/login" element = {<Login/>}/>
+       <Route path="/Register" element = {<Register/>}/>
 
    </Routes>
 
