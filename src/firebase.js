69315ebe71f7abc25cae5a8dad6e0f03ca41788b
HubKro <hubsonix@gmail.com>
2023-04-17 17:18:01 +0200

    Add login google

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "@firebase/firestore";
 import {getStorage} from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCdff6-Jm_Tg1w7KPLz4t8sjGoOBWp7V1s",
     authDomain: "custom-e30bd.firebaseapp.com",
@@ -19,8 +17,9 @@ const firebaseConfig = {
     measurementId: "G-QQCVYXJ86R"
 };
 
-// Initialize Firebase
+const provider = new GoogleAuthProvider();
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const storage = getStorage(app)
 export const firestore = getFirestore(app);
+export {provider};
