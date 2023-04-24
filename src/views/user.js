1ae17ea3c73f64a221c352ce06ee24710d5619d2
Pablito223 <portki14@gmail.com>
2023-04-24 16:37:16 +0200

    update homepage

--- src/views/user.js
+++ src/views/user.js
@@ -8,17 +8,64 @@ import Profile from "./Profile";
 function User(props) {
 
     return(
-    <div className="tlo-user">
-        <div className="obraz">
+        <div className="main">
+
+            <div className="nav">
+
+                <div className="logo-div">
+                    <a href="http://localhost:3000">
+                        <img src={logo} alt="Main.js Logo" className="logo-main" />
+                    </a>
+                    <div>
+                        <button className="ogloszenie-button">
+                            Dodaj Ogłoszenie
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="image-background">
+                <div>
+                    <button className="button1user">
+                        Konto
+                    </button>
+
+                    <button className="button2user">
+                        Ogłoszenia
+                    </button>
+
+                    <button className="button3user">
+                        Wiadomości
+                    </button>
+
+                    <button className="button4user">
+                        Płatności
+                    </button>
+                </div>
+            </div>
+            <div>
+                <button className="usernamech-button">
+                    Zmień Nazwę Użytkownika
+                </button>
+                <div>
+                <button className="passwordch-button">
+                    Zmień Hasło
+                </button>
+                </div>
+                <div>
+                    <button className="emailch-button">
+                        Zmień E-mail
+                    </button>
+                </div>
+                <div>
+                    <button className="infoch-button">
+                        Zaktualizuj informacje
+                    </button>
+                    
+                </div>
+            </div>
 
         </div>
-        <div className="logo-container">
-            <a href="http://localhost:3000">
-                <img src={logo} alt="Main.js Logo" className="logo_user"/>
-            </a>
-        </div>
 
-    </div>
     );
 }
 export default User;
